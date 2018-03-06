5701805ad71ca6616e198ac56920f4eb4910cf84
ASMITA RAJ <wonderboy@gmail.com>
2018-03-06 22:18:03 +0530

    modified wallet/urls.py

--- wallet/urls.py
+++ wallet/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^expense/', include('expense.urls', namespace='expense')),
+    url(r'^/', include('expense.urls', namespace='expense')),
 ]
 
 if settings.DEBUG:
